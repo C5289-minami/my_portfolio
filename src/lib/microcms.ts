db1bf09e576faedeab045598b243c002465ef5e1
Minami <C5289@oic.jp>
2026-03-29 08:51:40 +0900

    Final: Connect to microCMS

---
+++ src/lib/microcms.ts
@@ -0,0 +1,13 @@
+import { createClient } from "microcms-js-sdk";
+
+export const client = createClient({
+  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: import.meta.env.MICROCMS_API_KEY,
+});
+
+export type Blog = {
+  id: string;
+  title: string;
+  content: string;
+  eyecatch?: { url: string };
+};
