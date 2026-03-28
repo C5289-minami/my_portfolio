43e4511d247be42aafa4d375b2ea604cab3735f3
Minami <C5289@oic.jp>
2026-03-29 08:10:12 +0900

    Remove base path for troubleshooting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  // GitHub Pages用の設定
+  site: 'https://C5289-minami.github.io',
+ // base: '/my_portfolio',
+  integrations: [mdx(), sitemap()],
 });
