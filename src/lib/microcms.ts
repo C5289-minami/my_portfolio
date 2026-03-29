cbb47b5be58ee69b0cc20860f4c8e889eac53990
Minami <C5289@oic.jp>
2026-03-29 09:02:45 +0900

    Fix: Add static output to astro config

--- src/lib/microcms.ts
+++ src/lib/microcms.ts
@@ -4,3 +4,9 @@ export const client = createClient({
   serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN || '',
   apiKey: import.meta.env.MICROCMS_API_KEY || '',
 });
+
+// いったん「タイトル」だけに絞ってテスト
+export type Blog = {
+  id: string;
+  title: string;
+};
