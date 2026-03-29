997b1b6bf20159df59022879dd1a0096f6f92483
Minami <C5289@oic.jp>
2026-03-29 09:01:34 +0900

    Fix: Add static output to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  // GitHub Pages用の設定
-  site: 'https://C5289-minami.github.io',
- // base: '/my_portfolio',
-  integrations: [mdx(), sitemap()],
+	site: 'https://C5289-minami.github.io',
+	base: '/my_portfolio',
+	integrations: [mdx(), sitemap()],
+	output: 'static', // これを確認！
 });
