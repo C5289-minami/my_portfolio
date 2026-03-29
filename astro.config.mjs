91926f254d2884a1d533db9d65e92fef45d5d404
Minami <C5289@oic.jp>
2026-03-29 09:26:36 +0900

    Fix: Add base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
 
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-	site: 'https://C5289-minami.github.io',
-	base: '/my_portfolio',
-	integrations: [mdx(), sitemap()],
-	output: 'static', // これを確認！
+  // あなたのリポジトリ名を指定します
+  base: '/my_portfolio', 
+  site: 'https://c5289-minami.github.io',
 });
